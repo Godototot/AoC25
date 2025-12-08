6c476ac8d2c1d130720876f1a03a692c66e70e91
Godototot <red.godot@protonmail.com>
2025-12-08 14:52:01 +0100

    finished day 7 part 1

---
+++ code/day7.py
@@ -0,0 +1,81 @@
+import sys
+from helperfunc import *
+
+
+"""
+class Coordinates:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        if isinstance(other, Coordinates):
+            return self.x == other.x and self.y == other.y
+        return False
+
+
+def prepare_input(input_file):
+    grid_lines = read_input_lines(input_file)
+    start_point = Coordinates(0,0)
+    splitters = []
+    bottom = 0
+    for y in range(len(grid_lines)):
+        for x, grid_point in enumerate(grid_lines[y]):
+            if grid_point == 'S':
+                start_point = Coordinates(x, y)
+            elif grid_point == '^':
+                splitters.append(Coordinates(x, y))
+                bottom = y
+    return start_point, splitters, bottom
+"""
+
+def prepare_input(input_file):
+    grid_lines = read_input_lines(input_file)
+    splitters = []
+    start_x = grid_lines[0].index('S')
+    for grid_line in grid_lines:
+        splitters_in_line = []
+        for x, grid_point in enumerate(grid_line):
+            if grid_point == '^':
+                splitters_in_line.append(x)
+        splitters.append(splitters_in_line)
+    return start_x, splitters
+
+
+
+def part1(input_data):
+    start_x, splitters = input_data
+    split_counter = 0
+    tach_beams = {start_x}
+    for splitters_in_line in splitters:
+        new_tach_beams = set()
+        for beam in tach_beams:
+            if beam in splitters_in_line:
+                new_tach_beams.add(beam-1)
+                new_tach_beams.add(beam+1)
+                split_counter+=1
+            else:
+                new_tach_beams.add(beam)
+        tach_beams = new_tach_beams
+    return split_counter
+
+
+def part2(input_data):
+    return ''
+
+
+def main() -> None:
+    if len(sys.argv) > 2:
+        input_file = sys.argv[2]
+    else:
+        input_file = '../input/'+sys.argv[0][:-3]+'.txt'
+    if sys.argv[1] == '1':
+        print(part1(prepare_input(input_file)))
+    elif sys.argv[1] == '2':
+        print(part2(prepare_input(input_file)))
+    else:
+        raise Exception("Please clarify, which part you wanna execute.")
+
+
+if __name__ == '__main__':
+    main()
